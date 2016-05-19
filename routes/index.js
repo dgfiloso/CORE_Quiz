1bf6d585af5db19271b1728190d2115275c8dc53
David Gonzalez <dgfiloso95@gmail.com>
2016-05-19 22:17:23 +0200

    Tema 20: Gestionar usuarios

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var userController = require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,14 +12,13 @@ router.get('/', function(req, res, next) {
 
 // Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);	// autoload :quizId
+router.param('userId', userController.load);	// autoload :userId
 
 // Ruta a creditos
 router.get('/author', quizController.author);
 
 // Definición de rutas de /quizzes
-// router.get('/quizzes',							quizController.index);
 router.get('/quizzes.:format?',						quizController.index);
-//router.get('/quizzes/:quizId(\\d+)',				quizController.show);
 router.get('/quizzes/:quizId(\\d+).:format?',		quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check',			quizController.check);
 router.get('/quizzes/new',							quizController.new);
@@ -29,5 +29,14 @@ router.delete('/quizzes/:quizId(\\d+)',				quizController.destroy);
 
 router.get('/quizzes/:quizId(\\d+)/comments/new',	commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments',		commentController.create);
+
+// Definicion de rutas de cuenta
+router.get('/users',								userController.index);		// Listado de usuarios
+router.get('/users/:userId(\\d+)',					userController.show);		// Ver un usuario
+router.get('/users/new',							userController.new);		// Formulario sign in
+router.post('/users',								userController.create);		// Registrar usuario
+router.get('/users/:userId(\\d+)/edit',				userController.edit);		// Editar cuenta
+router.put('/users/:userId(\\d+)',					userController.update);		// Actualizar cuenta
+router.delete('/users/:userId(\\d+)',				userController.destroy);	// Borrar cuenta
 
 module.exports = router;
