a10b69356ad84f44b8f62dbeac6829ea92ef4e19
David Gonzalez <dgfiloso95@gmail.com>
2016-05-26 22:13:39 +0200

    Tema 23: Autorización

--- routes/index.js
+++ routes/index.js
@@ -20,29 +20,31 @@ router.get('/session',								sessionController.new);
 router.post('/session',								sessionController.create);
 router.delete('/session',							sessionController.destroy);
 
-// Ruta a creditos
-router.get('/author', 								quizController.author);
-
-// Definición de rutas de /quizzes
-router.get('/quizzes.:format?',						quizController.index);
-router.get('/quizzes/:quizId(\\d+).:format?',		quizController.show);
-router.get('/quizzes/:quizId(\\d+)/check',			quizController.check);
-router.get('/quizzes/new',							quizController.new);
-router.post('/quizzes',								quizController.create);
-router.get('/quizzes/:quizId(\\d+)/edit',			quizController.edit);
-router.put('/quizzes/:quizId(\\d+)',				quizController.update);
-router.delete('/quizzes/:quizId(\\d+)',				quizController.destroy);
-
-router.get('/quizzes/:quizId(\\d+)/comments/new',	commentController.new);
-router.post('/quizzes/:quizId(\\d+)/comments',		commentController.create);
-
 // Definicion de rutas de cuenta
 router.get('/users',								userController.index);		// Listado de usuarios
 router.get('/users/:userId(\\d+)',					userController.show);		// Ver un usuario
 router.get('/users/new',							userController.new);		// Formulario sign in
 router.post('/users',								userController.create);		// Registrar usuario
-router.get('/users/:userId(\\d+)/edit',				userController.edit);		// Editar cuenta
-router.put('/users/:userId(\\d+)',					userController.update);		// Actualizar cuenta
-router.delete('/users/:userId(\\d+)',				userController.destroy);	// Borrar cuenta
+router.get('/users/:userId(\\d+)/edit',				sessionController.loginRequired, userController.edit);		// Editar cuenta
+router.put('/users/:userId(\\d+)',					sessionController.loginRequired, userController.update);		// Actualizar cuenta
+router.delete('/users/:userId(\\d+)',				sessionController.loginRequired, userController.destroy);	// Borrar cuenta
+
+// Definición de rutas de /quizzes
+router.get('/quizzes.:format?',						quizController.index);
+router.get('/quizzes/:quizId(\\d+).:format?',		quizController.show);
+router.get('/quizzes/:quizId(\\d+)/check',			quizController.check);
+router.get('/quizzes/new',							sessionController.loginRequired, quizController.new);
+router.post('/quizzes',								sessionController.loginRequired, quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',			sessionController.loginRequired, quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',				sessionController.loginRequired, quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',				sessionController.loginRequired, quizController.destroy);
+
+router.get('/quizzes/:quizId(\\d+)/comments/new',	sessionController.loginRequired, commentController.new);
+router.post('/quizzes/:quizId(\\d+)/comments',		sessionController.loginRequired, commentController.create);
+
+// Ruta a creditos
+router.get('/author', 								quizController.author);
+
+
 
 module.exports = router;
